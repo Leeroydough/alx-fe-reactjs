efe86a1b344a4bf6caf8cd5cf104cb904d5b3f6d
Leeroydough <162184791+Leeroydough@users.noreply.github.com>
2024-08-11 21:08:23 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-import WelcomeMessage from './components/WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage';  // Import the WelcomeMessage component
 
 const App = () => {
   return (
