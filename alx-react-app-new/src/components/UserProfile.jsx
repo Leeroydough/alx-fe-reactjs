9118d5bb09045032597e577a9316f7f08ccaa332
Leeroydough <162184791+Leeroydough@users.noreply.github.com>
2024-08-18 17:31:49 +0200

    Create UserProfile.jsx

---
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -0,0 +1,14 @@
+// src/components/UserProfile.jsx
+import React from 'react';
+
+const UserProfile = (props) => {
+  return (
+    <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px' }}>
+      <h2 style={{ color: 'blue', fontSize: '28px', marginBottom: '10px' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold', fontSize: '18px' }}>{props.age}</span></p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile
