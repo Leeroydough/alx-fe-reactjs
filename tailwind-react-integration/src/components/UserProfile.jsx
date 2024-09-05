60b2baea7c4e5e7f61ce0eed2eac21f5009dd581
Leeroydough <162184791+Leeroydough@users.noreply.github.com>
2024-09-05 14:17:52 +0200

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 sm:my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
@@ -12,11 +12,12 @@ function UserProfile() {
         John Doe
       </h1>
       <p className="text-sm sm:text-base text-gray-600 text-center">
-        Developer at Home Co. Loves to write code and explore new technologies.
+        Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
   );
 }
 
 export default UserProfile;
+
 
